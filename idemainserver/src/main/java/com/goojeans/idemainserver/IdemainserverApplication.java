d49b5f8ee22f5e8d56d213b48c64aaeb1a989e40
Keon <lune0410k@naver.com>
2023-12-10 20:41:43 +0900

    Feat: add test files add test controller and make it login no needed by adding annotation option in main

--- idemainserver/src/main/java/com/goojeans/idemainserver/IdemainserverApplication.java
+++ idemainserver/src/main/java/com/goojeans/idemainserver/IdemainserverApplication.java
@@ -2,8 +2,9 @@ package com.goojeans.idemainserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
 public class IdemainserverApplication {
 
 	public static void main(String[] args) {
--- idemainserver/src/main/java/com/goojeans/idemainserver/controller/Initiate.java
+++ idemainserver/src/main/java/com/goojeans/idemainserver/controller/Initiate.java
@@ -1,4 +1,21 @@
 package com.goojeans.idemainserver.controller;
 
+import com.goojeans.idemainserver.domain.dto.response.TestResponseDto;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@Slf4j
+@RestController
 public class Initiate {
+
+    @GetMapping("/")
+    public TestResponseDto hello() {
+        RestTemplate restTemplate = new RestTemplate();
+
+        TestResponseDto responseDto = restTemplate.getForObject("http://runserver:8080/python", TestResponseDto.class);
+        log.info("response={}", responseDto);
+        return responseDto;
+    }
 }
