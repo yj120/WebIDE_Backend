804571003bed185e4df7d5bcdd04601e3b79d26c
Keon <lune0410k@naver.com>
2023-12-13 18:41:45 +0900

    Fix: fix to use testcontainer for test

--- idemainserver/src/test/java/com/goojeans/idemainserver/IdemainserverApplicationTests.java
+++ idemainserver/src/test/java/com/goojeans/idemainserver/IdemainserverApplicationTests.java
@@ -2,10 +2,18 @@ package com.goojeans.idemainserver;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.testcontainers.containers.JdbcDatabaseContainer;
+import org.testcontainers.containers.MySQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
+@Testcontainers
 @SpringBootTest
 class IdemainserverApplicationTests {
 
+	@Container
+	static JdbcDatabaseContainer testMysqlContainer = new MySQLContainer("mysql:8")
+			.withDatabaseName("testdb");
 	@Test
 	void contextLoads() {
 	}
