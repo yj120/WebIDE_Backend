4e1de0c8aaaf73120d6f8559d4d86538444dd1f2
Keon <lune0410k@naver.com>
2023-12-20 21:48:19 +0900

    Add: add util files add enum and config file

---
+++ idemainserver/src/main/java/com/goojeans/idemainserver/util/FileExtension.java
@@ -0,0 +1,5 @@
+package com.goojeans.idemainserver.util;
+
+public enum FileExtension {
+    PYTHON3, JAVA, CPP
+}
---
+++ idemainserver/src/main/java/com/goojeans/idemainserver/util/SubmitResult.java
@@ -0,0 +1,5 @@
+package com.goojeans.idemainserver.util;
+
+public enum SubmitResult {
+    CORRECT, WRONG, TIMEOUT, ERROR
+}
