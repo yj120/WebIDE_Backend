d5c3163da063dbd746db18d7110a5b1b9b825a78
goorm <honang515@gmail.com>
2023-12-20 19:26:36 +0900

    feat: controller add

---
+++ runserver/src/main/java/com/goojeans/runserver/controller/SubmitController.java
@@ -0,0 +1,35 @@
+package com.goojeans.runserver.controller;
+
+import com.goojeans.runserver.dto.request.SubmitRequestDto;
+import com.goojeans.runserver.dto.response.ApiResponse;
+import com.goojeans.runserver.dto.response.SubmitResponseDto;
+import com.goojeans.runserver.service.SubmitService;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/submit")
+public class SubmitController {
+
+    // TODO 일치하는 링크 없는 경우 에러로 메인으로 내려 주기. -> ControllerAdvice
+    private final SubmitService submitService;
+
+    @PostMapping
+    public ApiResponse submit(@RequestBody SubmitRequestDto submitRequestDto) {
+
+        // TODO Controller에서 ! 받을 수 있도록!! ApiResponse로 변환!!!!!
+        ApiResponse<SubmitResponseDto> submitResponseDto = submitService.codeJudge(submitRequestDto);
+
+        return submitResponseDto;
+
+    }
+
+}
---
+++ runserver/src/main/java/com/goojeans/runserver/dto/response/ApiResponse.java
@@ -0,0 +1,90 @@
+package com.goojeans.runserver.dto;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+public class ApiResponse<T> {
+
+
+	private int statusCode;
+
+	private List<T> data = new ArrayList<>();
+
+	private String message; // “오류 있다면 출력”
+
+	// public static ApiResponse<SubmitResponseDto> submitOk(List<SubmitResponseDto> data) {
+	// 	return new ApiResponse(200, data, null);
+	// }
+
+	public static ApiResponse<SubmitResponseDto> submitOk() {
+		List<SubmitResponseDto> datas = new ArrayList<>();
+		datas.add(SubmitResponseDto.ok());
+		return new ApiResponse<SubmitResponseDto>(200, datas, null);
+	}
+
+	public static ApiResponse<ExecuteResponseDto> executeOk(List<ExecuteResponseDto> data) {
+		return new ApiResponse<ExecuteResponseDto>(200, data, null);
+	}
+
+	public static ApiResponse<ExecuteResponseDto> executeOk(ExecuteResponseDto data) {
+		List<ExecuteResponseDto> datas = new ArrayList<>();
+		datas.add(data);
+		return new ApiResponse<ExecuteResponseDto>(200, datas, null);
+	}
+
+	// public static ApiResponse<SubmitResponseDto> submitNotOK(List<SubmitResponseDto> data) {
+	// 	return new ApiResponse<SubmitResponseDto>(200, data, null);
+	// }
+
+	public static ApiResponse<SubmitResponseDto> submitNotOK() {
+		List<SubmitResponseDto> datas = new ArrayList<>();
+		datas.add(SubmitResponseDto.notOk());
+		return new ApiResponse<SubmitResponseDto>(200, datas, null);
+	}
+	//
+	// public static ApiResponse<ExecuteResponseDto> executeNotOK(List<ExecuteResponseDto> data) {
+	// 	return new ApiResponse<ExecuteResponseDto>(200, data, null);
+	// }
+	//
+	// public static ApiResponse<SubmitResponseDto> submitError(List<SubmitResponseDto> data) {
+	// 	return new ApiResponse<SubmitResponseDto>(200, data, null);
+	// }
+
+	public static ApiResponse<SubmitResponseDto> submitError(String error) {
+		List<SubmitResponseDto> datas = new ArrayList<>();
+		datas.add(SubmitResponseDto.userCodeError(error));
+		return new ApiResponse<SubmitResponseDto>(200, datas, null);
+	}
+	//
+	// public static ApiResponse<ExecuteResponseDto> executeError(List<ExecuteResponseDto> data) {
+	// 	return new ApiResponse<ExecuteResponseDto>(200, data, null);
+	// }
+
+	// public static ApiResponse<SubmitResponseDto> submitServerError(List<SubmitResponseDto> data, String error) {
+	// 	return new ApiResponse<SubmitResponseDto>(200, data, error);
+	// }
+
+	public static ApiResponse<SubmitResponseDto> submitServerError(List<SubmitResponseDto> data, String error) {
+		return new ApiResponse<SubmitResponseDto>(6000, data, error);
+	}
+
+	public static ApiResponse<SubmitResponseDto> submitServerError(String error) {
+		return new ApiResponse<SubmitResponseDto>(6000, null, error);
+	}
+
+	public static ApiResponse<ExecuteResponseDto> executeServerError(List<ExecuteResponseDto> data, String error) {
+		return new ApiResponse<ExecuteResponseDto>(6000, data, error);
+	}
+
+}
