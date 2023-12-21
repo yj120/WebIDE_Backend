e84120ec79d8dbd63851c5bf784e72c079c3fd8f
Keon <lune0410k@naver.com>
2023-12-22 01:44:24 +0900

    Add: new Dto for file tree and remove message dto Remove message Dto to change FileTreeResponse and add new Dtos for file tree service

--- idemainserver/src/main/java/com/goojeans/idemainserver/domain/dto/response/FileResponses/FileTreeResponse.java
+++ idemainserver/src/main/java/com/goojeans/idemainserver/domain/dto/response/FileResponses/FileTreeResponse.java
@@ -7,7 +7,7 @@ import lombok.NoArgsConstructor;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-public class MessageResponse {
+public class FileTreeResponse {
 
-    private String message;
+    String path;
 }
