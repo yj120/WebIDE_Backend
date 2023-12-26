560623db653ef7a37e77b7cca0e470c6ad067dc4
goorm <honang515@gmail.com>
2023-12-26 20:12:43 +0900

    feat: solved languge 별 쿼리 생성

--- idemainserver/src/main/java/com/goojeans/idemainserver/repository/membersolved/MemberSolvedRepository.java
+++ idemainserver/src/main/java/com/goojeans/idemainserver/repository/membersolved/MemberSolvedRepository.java
@@ -2,6 +2,7 @@ package com.goojeans.idemainserver.repository.membersolved;
 
 import com.goojeans.idemainserver.domain.entity.MemberSolved;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface MemberSolvedRepository {
@@ -9,4 +10,7 @@ public interface MemberSolvedRepository {
     public Optional<MemberSolved> createMemberSolved(MemberSolved create);
 
     public Optional<MemberSolved> updateMemberSolved(MemberSolved update);
+
+
+    public List<Object[]> countSolvedByLanguage();
 }
--- idemainserver/src/main/java/com/goojeans/idemainserver/repository/membersolved/MemberSolvedRepositoryImpl.java
+++ idemainserver/src/main/java/com/goojeans/idemainserver/repository/membersolved/MemberSolvedRepositoryImpl.java
@@ -1,12 +1,15 @@
 package com.goojeans.idemainserver.repository.membersolved;
 
 import com.goojeans.idemainserver.domain.entity.MemberSolved;
+
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.TypedQuery;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Slf4j
@@ -14,36 +17,36 @@ import java.util.Optional;
 @Repository
 public class MemberSolvedRepositoryImpl implements MemberSolvedRepository {
 
-    private final EntityManager em;
+	private final EntityManager em;
 
-    @Override
-    public Optional<MemberSolved> createMemberSolved(MemberSolved create) {
-        em.persist(create);
+	@Override
+	public Optional<MemberSolved> createMemberSolved(MemberSolved create) {
+		em.persist(create);
 
-        return Optional.ofNullable(create);
-    }
+		return Optional.ofNullable(create);
+	}
 
-    @Override
-    public Optional<MemberSolved> updateMemberSolved(MemberSolved update) {
-        String jpql = "select m from MemberSolved m where m.algorithm = :algorithm and m.user = :user";
+	@Override
+	public Optional<MemberSolved> updateMemberSolved(MemberSolved update) {
+		String jpql = "select m from MemberSolved m where m.algorithm = :algorithm and m.user = :user";
 
-        TypedQuery<MemberSolved> query = em.createQuery(jpql, MemberSolved.class);
-        query.setParameter("algorithm", update.getAlgorithm());
-        query.setParameter("user", update.getUser());
-        MemberSolved findSolved;
-        try {
-            findSolved = query.getResultList().stream()
-                    .findAny()
-                    .orElseThrow(() -> new RuntimeException("cannot find memberSolved"));
-            log.info("check find solved={}", findSolved);
-        } catch (Exception e) {
-            em.persist(update);
-            return Optional.of(update);
-        }
+		TypedQuery<MemberSolved> query = em.createQuery(jpql, MemberSolved.class);
+		query.setParameter("algorithm", update.getAlgorithm());
+		query.setParameter("user", update.getUser());
+		MemberSolved findSolved;
+		try {
+			findSolved = query.getResultList().stream()
+				.findAny()
+				.orElseThrow(() -> new RuntimeException("cannot find memberSolved"));
+			log.info("check find solved={}", findSolved);
+		} catch (Exception e) {
+			em.persist(update);
+			return Optional.of(update);
+		}
 
-        findSolved.setSolved(update.getSolved());
-        findSolved.setLanguage(update.getLanguage());
+		findSolved.setSolved(update.getSolved());
+		findSolved.setLanguage(update.getLanguage());
 
-        return Optional.of(findSolved);
-    }
+		return Optional.of(findSolved);
+	}
 }
