a49a747d7304c1000d9aa1341213a0b50ba6f5a4
je326 <mya326@naver.com>
2023-12-27 11:53:55 +0900

    Fix: Fix Websocket connection

--- idemainserver/src/main/java/com/goojeans/idemainserver/util/WebSocketConfig.java
+++ idemainserver/src/main/java/com/goojeans/idemainserver/util/WebSocketConfig.java
@@ -21,7 +21,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
      */
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/ws/chat").setAllowedOriginPatterns("https://eb.goojeans-server.com")
+        registry.addEndpoint("/ws/chat").setAllowedOriginPatterns("*")
                 .withSockJS();
 
         //postman 테스트용 endpoint
